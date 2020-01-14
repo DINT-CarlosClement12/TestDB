74c96f181a1c7110062bf153fb7aa6bd051dd67c
CarlosClement12 <carlos.clement@iesdoctorbalmis.com>
2020-01-14 11:19:24 +0100

    Ave, Caesar

--- TestDB/MainWindow.xaml.cs
+++ TestDB/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,38 @@ namespace TestDB
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private BaseDeDatosCarlosClemenEntities ctx;
+        private CLIENTE _CLIENTE = new CLIENTE();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ctx = new BaseDeDatosCarlosClemenEntities();
+            ctx.CLIENTES.Load();
+
+            ClientsListBox.DataContext = ctx.CLIENTES.Local;
+            ClientsModifyComboBox.DataContext = ctx.CLIENTES.Local;
+            ClientsRemoveComboBox.DataContext = ctx.CLIENTES.Local;
+            InsertarStackPanel.DataContext = _CLIENTE;
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            ctx.CLIENTES.Add(_CLIENTE);
+            ctx.SaveChanges();
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            ctx.CLIENTES.Remove((CLIENTE)ClientsRemoveComboBox.SelectedItem);
+            ctx.SaveChanges();
+        }
+
+        private void ModificarButton_Click(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
         }
     }
 }
---
+++ TestDB/Partials/INotify.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace TestDB
+{
+    public partial class CLIENTE : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public partial class PEDIDO : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
